b23e22a3f3b2d194fe9d9ee6a65a683396fe3310
kmakrutin <k.makrutin@gmail.com>
2019-01-16 17:34:11 +0300

    #134 homework

--- CSS3/course-project/shared.js
+++ CSS3/course-project/shared.js
@@ -1,6 +1,10 @@
 var backdrop = document.querySelector( '.backdrop' );
 var modal = document.querySelector( '.modal' );
 var selectPlanButtons = document.querySelectorAll( '.plan button' );
+var selectCloseModalButton = document.querySelector( '.modal__action--negative' );
+
+selectCloseModalButton.addEventListener( 'click', closeModal );
+backdrop.addEventListener( 'click', closeModal );
 
 for ( var i = 0; i < selectPlanButtons.length; i++ )
 {
@@ -8,6 +12,12 @@ for ( var i = 0; i < selectPlanButtons.length; i++ )
     backdrop.style.display = 'block';
     modal.style.display = 'block';
   } );
+}
+
+function closeModal()
+{
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
 }
 
 
