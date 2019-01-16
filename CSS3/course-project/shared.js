11949828c3ba5a74deff8c2f7451a6eadcf29563
kmakrutin <k.makrutin@gmail.com>
2019-01-16 17:21:27 +0300

    #134

---
+++ CSS3/course-project/shared.js
@@ -0,0 +1,13 @@
+var backdrop = document.querySelector( '.backdrop' );
+var modal = document.querySelector( '.modal' );
+var selectPlanButtons = document.querySelectorAll( '.plan button' );
+
+for ( var i = 0; i < selectPlanButtons.length; i++ )
+{
+  selectPlanButtons[i].addEventListener( 'click', function ( event ) {
+    backdrop.style.display = 'block';
+    modal.style.display = 'block';
+  } );
+}
+
+
