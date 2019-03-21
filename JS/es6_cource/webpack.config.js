b969e921105874227e79d71cf17b26014b1f0e33
kmakrutin <k.makrutin@gmail.com>
2019-03-21 13:37:23 +0300

    #12 babel_config

--- JS/es6_cource/webpack.config.js
+++ JS/es6_cource/webpack.config.js
@@ -1,13 +1,22 @@
-const path = require('path');
+const path = require( 'path' );
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app'),
+  entry: path.resolve( __dirname, 'app' ),
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve( __dirname, 'build' ),
     filename: 'bundle.js'
   },
   devServer: {
     port: 3000,
-    contentBase: path.resolve(__dirname, 'build')
+    contentBase: path.resolve( __dirname, 'build' )
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
   }
 };
