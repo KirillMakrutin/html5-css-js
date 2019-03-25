8bac284423e436b8867ed063e512d1ded159a5eb
kmakrutin <k.makrutin@gmail.com>
2019-03-25 12:51:37 +0300

    #36 React fetch

--- JS/es6_cource/app/index.js
+++ JS/es6_cource/app/index.js
@@ -15,11 +15,41 @@ Frodo.greet();
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component{
+class App extends Component {
+  constructor( props )
+  {
+    super( props );
+    this.state = {
+      loaded: false,
+      title: '',
+      body: ''
+    }
+  }
+
+  componentDidMount()
+  {
+    fetch( 'https://jsonplaceholder.typicode.com/posts/1' )
+        .then( resp => resp.json() )
+        .then( json => this.setState( {
+          loaded: true,
+          title: json.title,
+          body: json.body
+        } ) )
+  }
+
   render()
   {
-    return <div>React JS and JSX in action</div>;
+    let display = <div>Loading...</div>;
+    if ( this.state.loaded )
+    {
+      display = <div>
+        <h1>{this.state.title}</h1>
+        <div>{this.state.body}</div>
+      </div>
+    }
+
+    return display;
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render( <App/>, document.getElementById( 'root' ) );
