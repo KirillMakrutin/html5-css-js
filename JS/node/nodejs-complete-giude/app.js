30331f7da27ff577dc322ea969ce4097c2b1f5c2
kmakrutin <k.makrutin@gmail.com>
2019-07-01 14:05:12 +0300

    Basic - send 1st response

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -1,8 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  const { url, method, headers } = req;
+  console.log("URL:", url);
+  console.log("METHOD:", method);
+  console.log("HEADERS:", headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`
+    <html>
+        <head><title>My first Page</title></head>
+        <body><h1>Hello from my Node.js</h1></body>
+    </html>
+  `);
+  res.end();
 });
 
 server.listen(3000);
