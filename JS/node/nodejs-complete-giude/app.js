a172b2f278d39c35d4262216976ea27c298d88f7
Kirill <k.makrutin@gmail.com>
2019-07-03 19:55:46 +0300

    #58 express.js installed

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -1,6 +1,7 @@
 const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express();
+const server = http.createServer(app);
 
 server.listen(3000);
