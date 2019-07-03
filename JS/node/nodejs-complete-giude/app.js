cc792bc640c2162c534de1a24d25bdc748284f12
Kirill <k.makrutin@gmail.com>
2019-07-03 20:02:39 +0300

    #60 express middleware concept

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -2,6 +2,15 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("In the midleware");
+  next();
+});
 
+app.use((req, res, next) => {
+  console.log("In another midleware");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+const server = http.createServer(app);
 server.listen(3000);
