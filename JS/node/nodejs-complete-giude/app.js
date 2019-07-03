34057784e65c1b7db6055e2573f167729b63639f
Kirill <k.makrutin@gmail.com>
2019-07-03 20:13:08 +0300

    #61 express use function

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 
 const app = express();
-app.use((req, res, next) => {
-  console.log("In the midleware");
+
+app.use("/", (req, res, next) => {
+  console.log("this always runs");
+
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In another midleware");
+app.use("/json", (req, res, next) => {
+  console.log("json");
+
   res.send({
     name: "Kirill",
     age: 33,
     some: "json"
   });
+});
+
+app.use("/", (req, res, next) => {
+  console.log("main");
+
+  res.send("<h1>Hello from Express</h1>");
 });
 
 app.listen(3000);
