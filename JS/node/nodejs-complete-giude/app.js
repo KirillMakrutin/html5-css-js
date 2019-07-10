39bd4091893ad05f27c7e63223c64fe110e4fe5d
Kirill Makrutin <k.makrutin@gmail.com>
2019-07-10 08:10:44 +0300

    Added handlebars engine

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -3,12 +3,15 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 const resource = require("./util/resource");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
-
-// it's a defualt value for views config varibale
+// for handlebars we had to register engine name, the name we like,
+// but template should have same extension, e.g. .hbs
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+// it's a default value for views config varibale
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +22,7 @@ app.use("/admin", adminData.routers);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", {pageTitle: "Error"});
+  res.status(404).render("404", { pageTitle: "Error", layout: false });
 });
 
 app.listen(3000);
