851b318a9b79d21795fa942f6694da77d9c2c58f
kmakrutin <k.makrutin@gmail.com>
2019-07-11 13:52:42 +0300

    #99 finished model with filesystem storage

--- JS/node/nodejs-complete-giude/models/product.js
+++ JS/node/nodejs-complete-giude/models/product.js
@@ -1,3 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const resource = require("../util/resource");
+
 const products = [];
 
 module.exports = class Product {
@@ -6,10 +10,31 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = resource("data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+
+      fs.writeFile(p, JSON.stringify(products), err => {
+        if (err) console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-      return products;
+  // callback argument
+  static fetchAll(cb) {
+    const p = resource("data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      if (!err) {
+        return cb(JSON.parse(fileContent));
+      } else {
+        return cb([]);
+      }
+    });
   }
 };
