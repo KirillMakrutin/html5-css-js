3ad9883a7ca25ef7d42ff0451180f4692c9670c8
Kirill <k.makrutin@gmail.com>
2019-07-14 13:48:43 +0300

    #162 CRUD operations with products by user

--- JS/node/nodejs-complete-giude/controllers/admin.js
+++ JS/node/nodejs-complete-giude/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  // sequelize automatically create associative methods 
+  // sequelize automatically create associative methods
   // but alternatively we can call Product.create and assign userId
   req.user
     .createProduct({
@@ -35,7 +35,15 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
+
+  req.user
+    .getProducts({
+      where: {
+        id: prodId
+      }
+    })
+    .then(products => products[0])
+    //Product.findByPk(prodId)
     .then(product => {
       if (product) {
         res.render("admin/edit-product", {
@@ -71,7 +79,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  //Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render("admin/products", {
         prods: products,
