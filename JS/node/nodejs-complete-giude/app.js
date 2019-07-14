63b6f55ed9c7f02c66d8a05e34c44d5325448954
Kirill <k.makrutin@gmail.com>
2019-07-14 13:39:06 +0300

    #160 Create assosiations between product and user and Create dummy user and put to all requests

--- JS/node/nodejs-complete-giude/app.js
+++ JS/node/nodejs-complete-giude/app.js
@@ -5,6 +5,8 @@ const errorController = require("./controllers/error");
 const bodyParser = require("body-parser");
 const resource = require("./util/resource");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -15,15 +17,45 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(resource("public")));
 
+// register middleware to have acces to dummy user
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(console.log);
+});
+
 // to handle all request to /admin/...
 app.use("/admin", adminRouters);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE"
+});
+
+// the next is options becuase of same above
+User.hasMany(Product);
+
 // Syncronize models to the databese, create tables
 sequelize
+  // will drop existing table and recreate them with the relations
+  //.sync({ force: true })
   .sync()
   .then(result => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Kirill", email: "admin@example.com" });
+    }
+    return user;
+  })
+  .then(user => {
+    console.log("Dummy admin user:", user);
     app.listen(3000);
   })
   .catch();
