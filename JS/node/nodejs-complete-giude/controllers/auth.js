f198b1071779cd0839509fc098c761ee5b2a6403
kmakrutin <k.makrutin@gmail.com>
2019-12-24 13:32:35 +0300

    #278 send reset password mesage

--- JS/node/nodejs-complete-giude/controllers/auth.js
+++ JS/node/nodejs-complete-giude/controllers/auth.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const sgMail = require("@sendgrid/mail");
+const crypto = require("crypto");
+
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // const nodemailer = require("nodemailer");
 // const sendGridTransport = require("nodemailer-sendgrid-transport");
@@ -124,5 +126,60 @@ exports.getReset = (req, res, next) => {
     path: "/reset",
     pageTitle: "Reset password",
     errorMessage: req.flash("error")
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  const { email } = req.body;
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      req.flash("error", "Cannot reset password.");
+      return res.redirect("/reset");
+    } else {
+      const token = buffer.toString("hex");
+      User.findOne({ email: email })
+        .then(userDoc => {
+          if (!userDoc) {
+            req.flash("error", "No account with the email.");
+            return res.redirect("/reset");
+          } else {
+            console.log("Reseting password for user", userDoc);
+
+            userDoc.resetToken = token;
+            userDoc.resetTokenExpiration = Date.now() + 3600;
+
+            return userDoc
+              .save()
+              .then(result => {
+                res.redirect("/login");
+
+                // return transporter.sendMail({
+                //   to: email,
+                //   from: "shop@node-complete-guide.com",
+                //   subject: "Singup succeeded!",
+                //   html: "<h1>Welcome on board!</h1>"
+                // });
+                console.log("Sending email to: ", email);
+                const msg = {
+                  to: email,
+                  from: "shop@node-complete.com",
+                  subject: "Password reset",
+                  html: `
+                <p>You requested password reset</p>
+                <p>Clicl this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+                `
+                };
+
+                console.log("Sending message: ", msg);
+
+                return sgMail.send(msg);
+              })
+              .then(result => console.log("The email has been sent"))
+              .catch(console.log);
+          }
+        })
+        .catch(console.log);
+    }
   });
 };
